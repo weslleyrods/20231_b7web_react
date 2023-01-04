04f5aea2d2078e7f9c95ed4bda2570b57c4d587a
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-01-04 19:05:38 -0300

    feat(modulo01): implementa props

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+const App = ()=>{
+
+  return (
+    <div>
+      <Header title="Cabeçalho com props!"/>
+      Olá, mundo!
+    </div>
+    );
+}
+
+export default App;
